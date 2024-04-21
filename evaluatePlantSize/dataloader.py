939c5509a232325b7b827522886b78c77acf1153
valaisu <63551725+valaisu@users.noreply.github.com>
2024-04-21 20:40:19 +0300

    Update dataloader
    add transformations

--- evaluatePlantSize/dataloader.py
+++ evaluatePlantSize/dataloader.py
@@ -1,16 +1,19 @@
+import os
 from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
 from PIL import Image
 
 
 """
-
+Creates the data loader, which contains batches 
 """
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, image_paths, label_paths):
+    def __init__(self, image_paths, label_paths, transform=None):
         self.image_paths = image_paths
         self.label_paths = label_paths
+        self.transform = transform
 
     def __len__(self):
         return len(self.image_paths)
@@ -18,16 +21,37 @@ class CustomImageDataset(Dataset):
     def __getitem__(self, idx):
         image = Image.open(self.image_paths[idx])
         label = Image.open(self.label_paths[idx])
+        if self.transform:
+            image = self.transform(image)
+            label = self.transform(label)
         return image, label
 
 
-image_paths = ['./data/img1.png', './data/img2.png', ...]
-label_paths = ['./data/label1.png', './data/label2.png', ...]
+def dataloader():
+    img_base = "C:\\Users\\valai\\PycharmProjects\\plantDetection\\images\\images\\"
+    lbl_base = "C:\\Users\\valai\\PycharmProjects\\plantDetection\\images\\labels\\"
+    image_paths = [img_base + i for i in os.listdir(img_base)]
+    label_paths = [lbl_base + i for i in os.listdir(lbl_base)]
 
-dataset = CustomImageDataset(image_paths=image_paths, label_paths=label_paths)
-data_loader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    dataset = CustomImageDataset(image_paths=image_paths, label_paths=label_paths, transform=transform)
+    data_loader = DataLoader(dataset, batch_size=16, shuffle=True, num_workers=4)
+    return data_loader
 
 
 
+# for testing
+def main():
+    data = dataloader()
+    for images, labels in data:
+        print(images.size)
+        print(labels.size)
+        print("")
 
+
+if __name__ == '__main__':
+    main()
 
